f7d6f44fda1b6454e38d170b9d0d972870ae7fa6
Aditya Gupta <adityapg@iisc.ac.in>
2025-04-14 22:51:20 +0530

    dropedge edits

--- model_dropedge.py
+++ model_dropedge.py
@@ -2,11 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GENConv
 
 torch.manual_seed(0)
 
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, edge_dim):
+        super().__init__()
+        self.conv1 = GENConv(in_channels, hidden_channels, edge_dim=edge_dim)
+        self.conv2 = GENConv(hidden_channels, out_channels, edge_dim=edge_dim)
 
+    def forward(self, x, edge_index, edge_attr=None):
+        x = self.conv1(x, edge_index, edge_attr)  # First convolution
+        x = F.relu(x)  # Apply ReLU activation
+        x = self.conv2(x, edge_index, edge_attr)  # Second convolution
+        return x
+    
 class DropoutGCN(torch.nn.Module):
     def __init__(self, input_dim, output_dim, edge_feature_dim, k, device, hidden1=16, hidden2=16, temperature=1.0):
         super(DropoutGCN, self).__init__()
@@ -17,9 +28,8 @@ class DropoutGCN(torch.nn.Module):
         self.output_dim = output_dim
         self.device = device
 
-        self.conv1 = GCNConv(input_dim, hidden1)
-        self.conv2 = GCNConv(hidden1, hidden2)
-        
+        self.conv = GCN(input_dim, hidden1, hidden2, edge_feature_dim)
+                
         self.fc = nn.Linear(hidden2, output_dim)
 
     def forward(self, num_nodes, edge_index, edge_attr, x, node_mask, training=True):
@@ -35,11 +45,9 @@ class DropoutGCN(torch.nn.Module):
         - training (bool): Training mode.
         """
         # Apply GCN layers
-        x = self.conv1(x, edge_index)  # First convolution
-        x = F.relu(x)  # Apply ReLU activation
-        x = self.conv2(x, edge_index)  # Second convolution
+        x = self.conv(x, edge_index, edge_attr=None)
         x = F.relu(x)
         x = self.fc(x)
         x = x[node_mask != 0]
-        
+
         return x
